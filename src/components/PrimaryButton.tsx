556ae991ea8ab3b407cb5fa25ae33fb3841c67cc
Mark Faust <markfaust123@gmail.com>
2023-12-31 01:56:49 -0500

    finished inital styling of StartGameScreen, now ready to implement initial game logic

---
+++ src/components/PrimaryButton.tsx
@@ -0,0 +1,46 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+const PrimaryButton = (props: any) => {
+  function handlePress() {
+    console.log("Pressed");
+  }
+
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={handlePress}
+        android_ripple={{ color: "#38021C" }}
+      >
+        <Text style={styles.buttonText}>{props.children}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063b",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
+
+export default PrimaryButton;
