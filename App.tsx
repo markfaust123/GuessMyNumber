3692aec97c1246f388959af6f4d717ae5a251e38
Mark Faust <markfaust123@gmail.com>
2023-12-31 20:49:33 -0500

    added SplashScreen while waiting for custom fonts to load

--- App.tsx
+++ App.tsx
@@ -1,14 +1,42 @@
 import { ImageBackground, SafeAreaView, StyleSheet } from "react-native";
 import StartGameScreen from "./src/screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import GameScreen from "./src/screens/GameScreen";
 import Colors from "./src/lib/constants";
 import GameOverScreen from "./src/screens/GameOverScreen";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [userNumber, setUserNumber] = useState<number>();
   const [gameIsOver, setGameIsOver] = useState<boolean>(true);
+
+  const [fontsLoaded, fontError] = useFonts({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+
+  // useEffect(() => {
+  //   async function hideSplashScreen() {
+  //     await SplashScreen.hideAsync();
+  //   }
+  //   if (fontsLoaded || fontError) {
+  //     hideSplashScreen();
+  //   }
+  // }, [fontsLoaded, fontError]);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError])
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
 
   function handlePickedNumber(pickedNumber: number) {
     setUserNumber(pickedNumber);
@@ -30,7 +58,7 @@ export default function App() {
   }
 
   return (
-    <LinearGradient colors={[Colors.primary700, Colors.accent500]} style={styles.rootScreen}>
+    <LinearGradient onLayout={onLayoutRootView} colors={[Colors.primary700, Colors.accent500]} style={styles.rootScreen}>
       <ImageBackground
         source={require("./assets/images/background.png")}
         resizeMode="cover"
